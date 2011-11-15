2405ae012c8bf16ea050413a28f3e5d023636b95
Fedor Indutny <fedor.indutny@gmail.com>
2011-11-15 17:48:26 +0600

    [tests] run sort-group engine on tests

--- test/fixtures/common.js
+++ test/fixtures/common.js
@@ -1,4 +1,5 @@
 var common = exports,
+    assert = require('assert'),
     path = require('path'),
     fs = require('fs'),
     xjst = require('../../lib/xjst');
@@ -7,5 +8,19 @@ common.render = function(name) {
   var filename = path.resolve(__dirname + '/../templates/' + name),
       template = fs.readFileSync(filename + '.xjst').toString();
 
-  return xjst.compile(template, name);
+  var sg = xjst.compile(template, name, { engine: 'sort-group' }),
+      fg = xjst.compile(template, name, { engine: 'fullgen' });
+
+  fg.apply.call = function(context) {
+    var results = [
+      sg.apply.call(context),
+      fg.apply.apply(context)
+    ];
+
+    assert.deepEqual(results[0], results[1]);
+
+    return results[0];
+  };
+
+  return fg;
 };
