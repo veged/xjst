ac97580e636672215d6348b381b3377b30c61d52
Fedor Indutny <fedor.indutny@gmail.com>
2012-06-13 16:09:36 +0400

    test: port super test to mocha

--- test/unit/super-test.js
+++ test/unit/super-test.js
@@ -2,10 +2,10 @@ var common = require('../fixtures/common'),
     fs = require('fs'),
     assert = require('assert');
 
-exports['super test'] = function(test) {
-  var apply = common.render('super-2').apply;
+suite('Super/extends', function () {
+  test('basic extension', function () {
+    var apply = common.render('super-2').apply;
 
-  assert.equal(apply.call({ name: 'main' }), 'ok');
-
-  test.done();
-};
+    assert.equal(apply.call({ name: 'main' }), 'ok');
+  });
+});
