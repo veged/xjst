b0c2ba374c78ae3b6c7a35fa2d35a82ae3e23713
Fedor Indutny <fedor.indutny@gmail.com>
2013-05-15 16:37:14 +0400

    xjst: fix nested locals

--- test/unit/local-test.js
+++ test/unit/local-test.js
@@ -14,6 +14,7 @@ suite('Local expressions/statements', function () {
   test('simple expression', localTest('local-expr', 'simple'));
   test('compelx expression', localTest('local-expr', 'complex'));
   test('regr-1 expression', localTest('local-expr', 'regr-1'));
+  test('regr-2 expression', localTest('local-expr', 'regr-2'));
 
   test('expression with hash', localTest('local-expr', 'hash'));
 
