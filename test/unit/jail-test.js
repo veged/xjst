7934e63e5a8d848a0bf00533a72bce65cfab9356
Fedor Indutny <fedor.indutny@gmail.com>
2013-05-17 13:20:20 +0400

    compiler: jail fixes

--- test/unit/jail-test.js
+++ test/unit/jail-test.js
@@ -17,6 +17,10 @@ describe('Compiler.jailVars', function() {
   unit('local var', 'var a', 'var a__$0;');
   unit('local var and use', 'var a;a', 'var a__$0;a__$0;');
   unit('local var and define', 'var a;a=1', 'var a__$0;a__$0=1;');
+  unit('local var and computed property', 'var x = 1;o[x]',
+       'var x__$0=1;o[x__$0];');
+  unit('local var and not-computed property', 'var x = 1;o.x',
+       'var x__$0=1;o.x;');
   unit('function with local use', 'function a() {var x = 1; return x}',
        'function a__$0(){var x=1;return x}');
   unit('function with outer use', 'var x = 1;function a() {return x}',
